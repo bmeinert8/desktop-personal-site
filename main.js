43c105088c953c1d5c669b6ae2f7845552270ba7
bmeinert8 <brian.meinert1@gmail.com>
2025-01-10 09:40:31 -0500

    fixed bug of theme toggle not working and time and date not working on windows display.

--- main.js
+++ main.js
@@ -9,20 +9,3 @@ document.querySelector('.js-nav-date')
   .innerHTML = 
   `<p>${time}</p>
   <p>${today}</p>`
-
-
-const theme_set = (toggled) => {
-  document.getElementById('style-windows').disabled = toggled;
-  document.getElementById('style-mac').disabled = !toggled;
-  localStorage.setItem('theme-toggled', toggled ? '1' : '');
-}
-
-const themeToggle = () => {
-  theme_set(!document.getElementById('style-windows').disabled);
-}
-
-theme_set(localStorage.getItem('theme-toggled'));
-
-const forgotPassword = () => {
-  alert('Type whatever you\'d like, or just press the arrow to sign in!');
-}
