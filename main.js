0e564d3534d736eee5fe7ff1411f2e92b43b664b
bmeinert8 <brian.meinert1@gmail.com>
2025-01-10 09:09:11 -0500

    updated windows home screen to display the date and time in the task bar

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
+const time = dayjs().format('h:mm A');
+const today = dayjs().format('MM/D/YYYY');
+console.log(today);
+console.log(time);
 
+document.querySelector('.js-nav-date')
+  .innerHTML = 
+  `<p>${time}</p>
+  <p>${today}</p>`
 
 
 const theme_set = (toggled) => {
